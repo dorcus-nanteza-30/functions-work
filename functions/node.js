eadf47b941ce56fd5a4a58fd037b8ba1e04df52f
dorcus-nanteza-30 <142330820+dorcus-nanteza-30@users.noreply.github.com>
2024-03-14 18:04:49 +0300

    Add files via upload

---
+++ functions/node.js
@@ -0,0 +1,26 @@
+// sum of two numbers.
+function sumNumbers(a1,a2){
+    return (a1+a2);
+}
+
+console.log(sumNumbers(303,56))
+
+
+
+
+/// square numbers///
+function square(num){
+    return num*num;
+}
+
+
+function squareNumbers(){
+     for (let i = 1; i <= 10; i++) {
+        let squared= i*i;
+        console.log("The square of " + i +" is: " + squared);
+    }
+}
+
+squareNumbers();
+
+
